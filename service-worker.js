975d8387cf4b097a1f3e4a0a820d02bec2deef5f
fwmc-ai <fwmc.ai@gmail.com>
2024-12-05 02:52:23 -0500

    FWMC-AI Radio v1.3.81

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fwmc-ai-radio-cache-v1.3.8'; // Update with new version when needed
+const CACHE_NAME = 'fwmc-ai-radio-cache-v1.3.81'; // Update with new version when needed
 const urlsToCache = [
   './',
   './index.html',
